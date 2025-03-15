cd2f35f46802780690256b9faba7abb23d3d0461
MrAwesomeOwl <140653655+MrAwesomeOwl@users.noreply.github.com>
2025-03-15 13:27:14 -0400

    beespolsion

--- scripts/objects/base_object.gd
+++ scripts/objects/base_object.gd
@@ -9,6 +9,18 @@ func lock_to_box(box: Node3D):
 	freeze = true
 	reparent(box,true)
 	
+func get_absorbed(by: Node3D) -> void:
+	var original_position = global_position
+	var original_size = scale
+	freeze = true
+	collision_layer = 0
+	collision_mask = 0
+	var i = 0
+	while i < 1:
+		i += .1
+		await Utils.wait(0.01)
+		global_position = lerp(original_position,by.global_position,i)
+		scale = lerp(original_size,Vector3.ZERO,i)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- scripts/objects/cat.gd
+++ scripts/objects/cat.gd
@@ -1,5 +1,7 @@
 extends BaseObject
 
+var bee_scene = preload("res://models/bee.blend")
+var process_delta = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,8 +9,22 @@ func _ready() -> void:
 	#$CollisionShape3D.shape = $CollisionShape3D.shape.duplicate()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	process_delta = delta
+		
+func emit_bee():
+	var bee = bee_scene.instantiate()
+	add_child(bee)
+	bee.top_level = true
+	bee.global_position = global_position
+	bee.global_rotation = Vector3(randf_range(-180,180),randf_range(-180,180),randf_range(-180,180))
+	var direction = Vector3(randf_range(-1,1),randf_range(-1,1),randf_range(-1,1)).normalized()
+	var size = .5
+	while size > 0.01:
+		await Engine.get_main_loop().process_frame
+		bee.global_position += direction * process_delta * 10
+		size = Utils.lerp_dt(size,0,.97,process_delta)
+		bee.scale = Vector3(size,size,size)
+		
 
 func _on_body_entered(body: Node) -> void:
 	print(body)
@@ -18,5 +34,10 @@ func _on_body_entered(body: Node) -> void:
 			$CollisionShape3D.scale += Vector3(0.05,0.05,0.05)
 			$Cat.scale += Vector3(0.05,0.05,0.05)
 		elif body.id == "can_of_bees":
-			body.queue_free()
-			queue_free()
+			await body.get_absorbed(self)
+			$AnimationPlayer.play("bee_explosion")
+			await Utils.wait(.5)
+			for i in range(20):
+				emit_bee()
+			#body.queue_free()
+			#queue_free()
